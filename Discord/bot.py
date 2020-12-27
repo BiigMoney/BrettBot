408ad07789b65ee13c6d4231c53c959cbab69f1b
BiigMoney <64580434+BiigMoney@users.noreply.github.com>
2020-12-27 07:58:44 -0800

    hffh

--- Discord/bot.py
+++ Discord/bot.py
@@ -63,7 +63,7 @@ async def on_message(message):
                 if not word.lower() in title.lower():
                     lol = False
             if not lol:
-                print("not lol here " + str(lol) + " searchwords " + str(searchwords))
+                print("not lol here " + str(lol) + " searchwords " + str(searchwords) + " " + title.lower())
                 continue
             for i in range(keywords.length):
                 print("entering keyword loop")
