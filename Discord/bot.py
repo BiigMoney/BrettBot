99fc2b0f69fefe449b5e9abe848d45029d8ddbb5
BiigMoney <64580434+BiigMoney@users.noreply.github.com>
2021-01-02 15:15:13 -0800

    adgs

--- Discord/bot.py
+++ Discord/bot.py
@@ -30,7 +30,9 @@ async def on_ready():
             data = file.read()
             currentTourney = json.loads(data)
             print(str(currentTourney))
-            currentChallongeTourney = challonge.tournaments.show(currentTourney['link'].rsplit('/', 1)[-1])
+            url = currentTourney['link'].rsplit('/', 1)[-1]
+            print(url)
+            currentChallongeTourney = challonge.tournaments.show(url)
             print(str(currentTourney))
             print(str(currentChallongeTourney))
     called_once_a_min.start()
